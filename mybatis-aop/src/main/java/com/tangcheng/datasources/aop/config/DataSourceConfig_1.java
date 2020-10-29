0600e0b66c5a33a4f92653d1377c873be2e154bf
cheng.tang <cheng.tang@zkh360.com>
2020-10-29 14:30:56 +0800

    upgrade spring boot version and replace old api

--- mybatis-aop/src/main/java/com/tangcheng/datasources/aop/config/DataSourceConfig_1.java
+++ mybatis-aop/src/main/java/com/tangcheng/datasources/aop/config/DataSourceConfig_1.java
@@ -1,8 +1,8 @@
 package com.tangcheng.datasources.aop.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
